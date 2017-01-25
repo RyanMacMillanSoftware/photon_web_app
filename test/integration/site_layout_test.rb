45a79e477cedacd5e9ffc10fb652866bcdad051c
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-01-26 09:13:51 +1300

    .

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,7 +10,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
 	 get contact_path
     assert_select "title", full_title("Contact")
   end
+  
+ 
 end
