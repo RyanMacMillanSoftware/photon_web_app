85a5bcf8f0889e894b064520d6a311f8018382b8
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-07 12:18:23 +1300

    admins exclusivly add users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :admin_user,     only: [:destroy, :create, :new]
    
   def destroy
     User.find(params[:id]).destroy
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to login_path and return unless @user.activated?
   end
+  
   def new
   	@user = User.new
   end
@@ -26,8 +27,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
     	@user.send_activation_email
-    	flash[:info] = "Please check your email to activate your account."
-      redirect_to login_path
+    	flash[:info] = "An email for account activation has been sent to the user"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -56,6 +57,8 @@ class UsersController < ApplicationController
     end
 
     # Before filters
+    
+    
 
     # Confirms a logged-in user.
     def logged_in_user
@@ -74,6 +77,10 @@ class UsersController < ApplicationController
     
     # Confirms an admin user.
     def admin_user
-      redirect_to(root_url) unless current_user.admin?
+    	if current_user.nil?
+      	redirect_to(root_url)
+      else
+      	redirect_to(root_url) unless current_user.admin?
+      end
     end
 end
