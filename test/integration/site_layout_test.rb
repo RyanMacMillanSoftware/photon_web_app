a7e40590649b0ae1e2b5b357fa0feac15f10878f
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-07 10:24:29 +1300

    Deployable Page Content

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links logged in" do
   	log_in_as(@user)
     get root_path
-    assert_template 'sessions#new'
+    assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
