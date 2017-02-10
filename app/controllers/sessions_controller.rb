0f09460e9efcfbd3bcc9ee96f39a3ef3753dd3f5
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-10 14:45:42 +1300

    create sessions fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if @user 
-    	if (@user.temporary_active && (@user.temporary_password == params[:session][:password])) || @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
      	 if @user.activated?
         log_in @user
         params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
@@ -21,7 +20,6 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
      end
-    end
   end
 
   def destroy
