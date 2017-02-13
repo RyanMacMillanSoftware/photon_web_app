5b527329eb8e2acbe95e62ecfa49182cdac8cad7
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-13 14:06:08 +1300

    trialing mailer changes in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
+    :from => 'noreply@photon-factory.herokuapp.com'
   }
   
   
