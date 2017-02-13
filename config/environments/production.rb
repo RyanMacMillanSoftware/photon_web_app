85014c91d7c4d423228b3089902e7f56641ce5a3
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-13 20:28:24 +1300

    changed host address for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
 	config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-scrubland-44517.herokuapp.com'
+  host = 'photon-factory.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
