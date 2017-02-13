5dde28951748273c8fab979466591bf4e048327a
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-02-13 20:14:54 +1300

    FIXED MAILER in last commit. This commit trying other email address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
 		
-	#default  from: 'noreply@photon-factory.herok#uapp.com'
-	default  from: 'from@example.com'
+	default  from: 'noreply@photon-factory.herokuapp.com'
+	#default  from: #'from@example.com'
 	layout 'mailer'
 end
