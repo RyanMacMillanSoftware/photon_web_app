7dc977b67815c9cb2358feeaa2a93cd7305a82be
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-03-28 14:59:30 +1300

    added student and staff to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313202540) do
+ActiveRecord::Schema.define(version: 20170328014802) do
 
   create_table "check_ins", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20170313202540) do
     t.string   "temporary_password"
     t.boolean  "temporary_active"
     t.boolean  "microfab"
+    t.boolean  "student"
+    t.boolean  "staff"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
