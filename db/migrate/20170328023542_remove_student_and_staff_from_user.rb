d5e69719fa74c1dfcfd1c8fbd2ef6639b6be20ef
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-04-20 13:19:39 +1200

    migration error fix

--- db/migrate/20170328023542_remove_student_and_staff_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveStudentAndStaffFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :student, :boolean
-    remove_column :users, :staff, :boolean
-  end
-end
