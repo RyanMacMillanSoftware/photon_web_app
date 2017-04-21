8d2cdeb0eba62df941fc64732f6b265ea3f7fdaa
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-04-21 14:58:33 +1200

    fixed LabLogin view. Punch to Login view change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  validate :permission_either_student_or_staff
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -71,13 +70,6 @@ class User < ApplicationRecord
   
 	private
 
-	def permission_either_student_or_staff
-      return if permission.blank?
-
-  		if permission != "Student" || permission != "Staff"
-    		errors.add(:to_time, "not a valid permissions type") 
-  		end 
-	end 
 		
     # Converts email to all lower-case.
     def downcase_email
