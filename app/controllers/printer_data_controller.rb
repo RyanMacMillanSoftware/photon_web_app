0916ba1ab8a023aaa978c132283c963e75790040
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2017-07-19 11:48:16 +1200

    added model validation. controller skeleton. TODO: add phone number to model

--- app/controllers/printer_data_controller.rb
+++ app/controllers/printer_data_controller.rb
@@ -1,2 +1,48 @@
 class PrinterDataController < ApplicationController
+	#The ugliest controller in the codebase
+#Creates CheckIns, turns CheckIns into TimePunches. 
+
+
+	
+	before_action :logged_in_user
+   
+	def new
+  		@printer_data = PrinterData.new
+	end
+  
+
+	def create
+		
+	end
+
+	def index
+
+	end
+
+   private
+
+    # Before filters
+
+    # Confirms an admin user.
+    def admin_user
+    	if current_user.nil?
+      	redirect_to(login_path)
+      else
+      	redirect_to(login_path) unless current_user.admin?
+      end
+    end
+    
+	# Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+      	store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+   
+    
+    
+end
+
 end
