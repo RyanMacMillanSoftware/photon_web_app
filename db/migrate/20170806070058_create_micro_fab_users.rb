c04b6c13b35280f9df91f72f41d6930c0067f549
Ryan MacMillan <rmac460@aucklanduni.ac.nz>
2017-08-10 12:34:21 +1200

    DELETED MIGRATIONS so dev env can migrate. header changes to allow non-users to navigate to printer status.

--- db/migrate/20170806070058_create_micro_fab_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateMicroFabUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :micro_fab_users do |t|
-      t.string :name
-      t.string :email
-    end
-  end
-end
--- db/migrate/20170808032651_add_guest_name_to_time_punch.rb
+++
@@ -1,5 +0,0 @@
-class AddGuestNameToTimePunch < ActiveRecord::Migration[5.0]
-  def change
-    add_column :time_punches, :guest_name, :string
-  end
-end
