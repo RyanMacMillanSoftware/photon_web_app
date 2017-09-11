a21a58885510f88a41f0b9ae08dadc9ac92d5b87
Ryan MacMillan <rmac460@aucklanduni.ac.nz>
2017-09-11 12:00:41 +1200

    fixed current_user bug in printer_statuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720222843) do
+ActiveRecord::Schema.define(version: 20170809232147) do
 
   create_table "check_ins", force: :cascade do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170720222843) do
     t.time     "completion_time"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
   create_table "selections", force: :cascade do |t|
