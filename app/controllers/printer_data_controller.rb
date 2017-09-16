f7c21c070ef8f5818f1d1de43ea5f1810dbdaef7
Ryan MacMillan <rmac460@aucklanduni.ac.nz>
2017-09-16 16:39:20 +1200

    Changing printer data to be from a specific printer (no dropdown). View works. Controller broken. Passing printer not solved

--- app/controllers/printer_data_controller.rb
+++ app/controllers/printer_data_controller.rb
@@ -15,10 +15,10 @@ class PrinterDataController < ApplicationController
       hour = hour + 12 unless hour == 12
     end
     to_time = DateTime.now.change(hour: hour, min: params[:'minute'].to_i, sec: 1)
-   		@printer_data = PrinterDatum.new(name: params[:printer_datum][:'name'], project: params[:printer_datum][:'project'], printer:params[:printer_datum][:'printer'], phonenumber: params[:printer_datum][:'phonenumber'], volume: params[:printer_datum][:'volume'], notes: params[:printer_datum][:'notes'], from_time: DateTime.now, to_time: to_time)
+   		@printer_data = PrinterDatum.new(name: params[:printer_datum][:'name'], project: params[:printer_datum][:'project'], printer: PrinterStatus.find(params[:printer_id]), phonenumber: params[:printer_datum][:'phonenumber'], volume: params[:printer_datum][:'volume'], notes: params[:printer_datum][:'notes'], from_time: DateTime.now, to_time: to_time)
     	if @printer_data.save
         #update printer status
-        printer = PrinterStatus.find_by(printer: @printer_data.printer)
+        printer = PrinterStatus.find(params[:printer_id])
         printer.update_attributes(available: false, completion_time: to_time, name: params[:printer_datum][:'name'], number: params[:printer_datum][:'phonenumber'])
     		flash[:success] = "Data Stored Successfully"
     		redirect_to printer_statuses_path
--- app/models/printer_datum.rb
+++ app/models/printer_datum.rb
@@ -2,7 +2,7 @@ class PrinterDatum < ApplicationRecord
 
 validates :name,  presence: true, length: { maximum: 50 }
 validates :project,  presence: true, length: { maximum: 50 }
-validates :printer,  presence: true, length: { maximum: 50 }
+#validates :printer,  presence: true, length: { maximum: 50 }
 validates :phonenumber, :numericality => {:only_integer => true}
 validates :to_time, presence: true
 validates :volume, :numericality => {:only_integer => true}
