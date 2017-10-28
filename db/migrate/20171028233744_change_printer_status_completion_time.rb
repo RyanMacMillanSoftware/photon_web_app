b259afffde6da4d5b4fe11e994a04f629055ca0d
Ryan MacMillan <rmac460@aucklanduni.ac.nz>
2017-10-29 12:58:48 +1300

    migration to remove then readd completiontime to PrinterStatuses

--- db/migrate/20171028233744_change_printer_status_completion_time.rb
+++
@@ -1,5 +0,0 @@
-class ChangePrinterStatusCompletionTime < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :printer_statuses, :completion_time, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028233744) do
+ActiveRecord::Schema.define(version: 20171028235819) do
 
   create_table "check_ins", force: :cascade do |t|
     t.string   "name"
@@ -72,12 +72,12 @@ ActiveRecord::Schema.define(version: 20171028233744) do
   create_table "printer_statuses", force: :cascade do |t|
     t.string   "printer"
     t.boolean  "available"
-    t.datetime "completion_time"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "name"
     t.string   "number"
     t.string   "image"
+    t.datetime "completion_time"
   end
 
   create_table "selections", force: :cascade do |t|
