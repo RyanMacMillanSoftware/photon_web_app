b74d2d039c65d7592ecd6a114e7f31e6c277a7b4
Ryan MacMillan <rmac460@aucklanduni.ac.nz>
2018-06-26 15:59:04 +1200

    Printer volume no longer compulsory

--- app/models/printer_datum.rb
+++ app/models/printer_datum.rb
@@ -5,8 +5,6 @@ validates :project,  presence: true, length: { maximum: 50 }
 #validates :printer,  presence: true, length: { maximum: 50 }
 validates :phonenumber, :numericality => {:only_integer => true}
 validates :to_time, presence: true
-validates :volume, :numericality => {:only_integer => true}
-
 
 end
 
